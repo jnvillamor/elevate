43707f2193475c7314a86c512334118ba41229d1
-m <villamorjohnnoel@gmail.com>
2023-12-21 14:18:21 +0800

    Fix the logo sizes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
     <nav className='py-6 px-16 bg-neutrals-950 flex justify-between items-center'>
       <div className='flex gap-2'>
         <Link href='/'>
-          <Image src='/logo.svg' alt='Logo' width={51} height={48} />
+          <Image src='/logo.svg' alt='Logo' width={45} height={48} />
         </Link>
-        <Image src='/logo-letter.svg' alt='Logo' width={200} height={46} />
+        <Image src='/logo-letter.svg' alt='Logo' width={150} height={46} />
       </div>
       <NavLinks />
     </nav>
