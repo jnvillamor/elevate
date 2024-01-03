5f4d18d61ddf0e8053eb491ed53312806844f108
-m <villamorjohnnoel@gmail.com>
2024-01-03 15:08:24 +0800

    Add temporary loading screens

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-neutrals-950'>
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
+export default loading
